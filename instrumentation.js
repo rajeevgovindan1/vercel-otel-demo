83775056b59ad3bdd805ff0ecad2f591a9ca89a8
Rajeev Govindan <rajeev.govindan@coralogix.com>
2025-05-20 15:45:22 -0500

    Finalize Next.js Otel instrumentation with Vercel

--- instrumentation.js
+++ instrumentation.js
@@ -1,7 +1,15 @@
 const { registerOTel } = require('@vercel/otel');
+const { Resource } = require('@opentelemetry/resources');
 
 module.exports.register = function () {
   console.log('🚀 OTel instrumentation initialized');
-  registerOTel({ serviceName: 'vercel-otel-demo' });
+
+  registerOTel({
+    serviceName: 'vercel-otel-demo',
+    resource: new Resource({
+      'cx.application.name': 'vercel-otel-app',
+      'cx.subsystem.name': 'api',
+    }),
+  });
 };
 
